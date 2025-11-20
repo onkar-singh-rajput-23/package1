e16e4611b8646c3518cba8bbe3d925a96ba559a2
Onkar Singh Rajput <onkar.rajput@travenues.com>
2025-11-20 10:12:29 +0530

    created swift updated

--- Package.swift
+++ Package.swift
@@ -5,13 +5,28 @@ let package = Package(
     name: "Package",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v13)
+        .iOS(.v13)
     ],
     products: [
-        .executable(
+        .iOSApplication(
             name: "PackageApp",
-            targets: ["PackageApp"]
+            targets: ["PackageApp"],
+            bundleIdentifier: "com.onkarrajput.package",
+            teamIdentifier: "YOURTEAMID",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            appIcon: .asset("AppIcon"),
+            accentColor: .asset("AccentColor"),
+            supportedDeviceFamilies: [
+                .phone,
+                .pad
+            ],
+            supportedInterfaceOrientations: [
+                .portrait,
+                .portraitUpsideDown(.when(deviceFamilies: [.pad])),
+                .landscapeLeft,
+                .landscapeRight
+            ]
         )
     ],
     targets: [
