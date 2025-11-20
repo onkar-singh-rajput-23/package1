7a229b329d98321cb1b1e51269caa045b05336ad
Onkar Singh Rajput <onkar.rajput@travenues.com>
2025-11-20 09:54:33 +0530

    Initial Commit

---
+++ Package/PackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackageApp.swift
+//  Package
+//
+//  Created by onkar.rajput on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PackageTests/PackageTests.swift
@@ -0,0 +1,16 @@
+//
+//  PackageTests.swift
+//  PackageTests
+//
+//  Created by onkar.rajput on 20/11/25.
+//
+
+import Testing
+
+struct PackageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
