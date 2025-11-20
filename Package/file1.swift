9a85be9021b103ac758c569af19c0d7126387e2e
Onkar Singh Rajput <onkar.rajput@travenues.com>
2025-11-20 09:57:42 +0530

    first commit

---
+++ Package/file1.swift
@@ -0,0 +1,18 @@
+//
+//  file1.swift
+//  Package
+//
+//  Created by onkar.rajput on 20/11/25.
+//
+
+import SwiftUI
+
+struct file1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    file1()
+}
